47ed03fb04e7dd4adb538b452b28bd1d0a93a5ee
pauloortins <paulo.ortins@gmail.com>
2014-04-04 23:11:07 -0300

    Added example using dojo.js file (added module exports)

--- boilerplates/js/lib/dojo.js
+++ boilerplates/js/lib/dojo.js
@@ -1,5 +1,9 @@
+
+
 var dojo = {
 	run: function(romanNumer) {
 		return "1"
 	}
 };
+
+module.exports = dojo;
--- boilerplates/js/test/tests.js
+++ boilerplates/js/test/tests.js
@@ -1,9 +1,10 @@
+
 var should = require('should');
 var dojo = require('../lib/dojo');
 
 
 describe('testando operações matemáticas', function() {
-  it('1 + 1 deve ser igual a 2', function() {
-  	(1 + 1).should.be.exactly(2);
+  it('1 + 1 deve ser igual a 2', function() {	
+  	(1 + 1).should.be.exactly(dojo.run(1));
   });
 });
