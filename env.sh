3f6166ac463de216d1f11f70e7558f36450c1176
pauloortins <paulo.ortins@gmail.com>
2014-04-05 13:55:28 -0300

    Delete extra shellscript

--- env.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install nodejs -y
-sudo apt-get install npm -y
-sudo npm config set registry http://registry.npmjs.org/
-sudo chdir=/vagrant npm install mocha -g
-sudo chdir=/vagrant npm install should
